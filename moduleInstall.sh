1ac1fdac40aa4b348bcc3aa9cb0604fb1c7c35d0
s4nj1_0s0b4@protonmail.com <s4nj1_0s0b4@protomail.com>
2021-05-06 14:08:32 +0200

    Ajout d'un script pour l'installation des modules neccessaires

---
+++ moduleInstall.sh
@@ -0,0 +1,11 @@
+#!/bin/bash 
+
+sudo apt-get update
+sudo pip3 install scapy 
+sudo apt install libmariadb3 libmariadb-dev
+sudo pip3 install mariadb
+sudo apt-get install build-essential python-dev libnetfilter-queue-dev
+pip3 install nfqp3
+sudo apt-get install mariadb-server
+sudo install nfqp3
+sudo mysql_secure_installation 
